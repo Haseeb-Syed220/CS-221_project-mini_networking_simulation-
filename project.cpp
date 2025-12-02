cdd8f9edc9de335c24cae7d2d49ff9da8ec5d932
Rehman047 <u2024026@giki.edu.pk>
2025-12-02 20:40:19 +0500

    Added the class UserNode which represents people in the network. It is linked list style

--- project.cpp
+++ project.cpp
@@ -1,5 +1,17 @@
 #include<iostream>
 using namespace std;
+class UserNode{
+    int id;
+    string username;
+    UserNode* next;
+    UserNode* friends;
+    public:
+    UserNode(int id, string un){
+        id=id;
+        username=un;
+    }
+};
+
 int main(){
-    cout<<"Start";
+
 }
