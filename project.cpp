d31588e8b67fdd0b53470729660d4e67e4ff1148
Rehman047 <u2024026@giki.edu.pk>
2025-12-02 21:23:21 +0500

    Added some functionality in User Linked List

--- project.cpp
+++ project.cpp
@@ -3,9 +3,9 @@ using namespace std;
 class UserNode{
     int id;
     string username;
-    UserNode* next;
     FriendNode* friends;
     public:
+     UserNode* next;
     UserNode(int uid, string un){
         id=uid;
         username=un;
@@ -20,7 +20,27 @@ class FriendNode{
         next=nullptr;
     }
 };
-
+class UserList{
+UserNode* head;
+static int count;
+public:
+UserList(){
+    head=nullptr;
+}
+void add_user(string un){
+    UserNode* node=new UserNode(count,un);
+    if(head==nullptr){
+        head=node;
+        return;
+    }
+    UserNode* temp=head;
+    while(temp->next!=nullptr)
+    temp=temp->next;
+    temp->next=node;
+    count++;
+}
+};
+int UserList::count=0;
 int main(){
 
 }
